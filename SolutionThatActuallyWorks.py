8e43d75484bc4a4e26c10633e8a4964acb357e5e
KuskIV <mkusk18@student.aau.dk>
2020-04-09 16:47:25 +0200

    titration

--- SolutionThatActuallyWorks.py
+++ SolutionThatActuallyWorks.py
@@ -6,17 +6,17 @@ from itertools import count
 from threading import Timer
 import warnings
 
-
-amount = 5
-
 index = count()
-
-caMa = [[] for i in range(amount)]
 stepList = []
 iter = [100, 20, 100, 100, 100]
 name = ['A', 'B', 'C', 'D', 'E']
-style = ['g-','ro-','b:','y-.', 'b--']
-pre_val = [100,90,80,70,60]
+
+sDict = {"A":[100],
+         "B":[90],
+         "C":[80],
+         "D":[70],
+         "E":[60]
+}
 
 ft = 100            #final t
 h = 0.00125         #step size
@@ -24,8 +24,6 @@ t = np.arange(0,ft + 0.5,1)
 n = len(t)
 
 def oneEuler(val) :
-    old = val.copy()
-    
     k1 = 1  #Alpha
     k2 = 1  #Beta
     k3 = 1  #Gamme
@@ -34,39 +32,53 @@ def oneEuler(val) :
     k6 = 1  #Zeta
     k7 = 1  #Theta
 
-    r1 = k1 * val[0]
-    r2 = k2 * val[1]
-    r3 = k3 * val[0] * val[2]
-    r4 = k4 * val[3]
-    r5 = k5 * val[1] * val[4]
-    r6 = k6 * val[1] * val[4]
-    r7 = k7 * val[4]
+    r1 = k1 * val.get("A")[-1]
+    r2 = k2 * val.get("B")[-1]
+    r3 = k3 * val.get("A")[-1] * val.get("C")[-1]
+    r4 = k4 * val.get("D")[-1]
+    r5 = k5 * val.get("B")[-1] * val.get("E")[-1]
+    r6 = k6 * val.get("B")[-1] * val.get("E")[-1]
+    r7 = k7 * val.get("E")[-1]
 
-    val[0] = (-r1 + r2 - r3 + r6) * h + old[0]
-    val[1] = (r1 - r2 + r4 - r5 - r6) * h + old[1]
-    val[2] = (-r3 + r6 + r7) * h + old[2]
-    val[3] = (r3 - r4 + r5 ) * h + old[3]
-    val[4] = (r4 - r7 - r6) * h + old[4]
+    val["A"].append((-r1 + r2 - r3 + r6) * h + val.get("A")[-1])
+    val["B"].append((r1 - r2 + r4 - r5 - r6) * h + val.get("B")[-1])
+    val["C"].append((-r3 + r6 + r7) * h + val.get("C")[-1])
+    val["D"].append((r3 - r4 + r5 ) * h + val.get("D")[-1])
+    val["E"].append((r4 - r7 - r6) * h + val.get("E")[-1])
 
     return val
 
+def applyTitration(pre_val, i):
+    val = pre_val.copy()
+
+    if i % 2 == 0:
+        val["A"][-1] = val.get("A")[-1] + 1
+
+    if i % 5 == 0:
+        if val.get("A")[-1] - 100 <= 0 :
+            val.get("A")[-1] = 0 
+        else :
+            val["A"][-1] = val.get("A")[-1] - 100
+
+    return val
+     
 
 def animate(i) :
-    global pre_val
+    global sDict
 
-    pre_val = oneEuler(pre_val)
     plt.cla()
-    
     ax1 = plt.subplot(212)
     ax1.grid(True, linestyle='-.')
     ax2 = plt.subplot(211)
+
     stepList.append(next(index))
-    for j in range(0, amount):
-        if i <= iter[j] :
-            caMa[j].append(pre_val[j])
-        ax1.plot(stepList, caMa[j], style[j] ,label=name[j])
-        ax2.plot(stepList, caMa[j], style[j] ,label=name[j])
+    for key, value in sDict.items():
+        ax1.plot(stepList, value, label=key)
+        ax2.plot(stepList, value, label=key)
         plt.legend()
+    
+    sDict = oneEuler(sDict)
+    sDict = applyTitration(sDict, i+1)
 
 ani = FuncAnimation(plt.gcf(), animate, interval=1000)
 
